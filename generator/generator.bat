a5475c29113395c0073e0de3b46aa22b390976a9
meli <57847713+ff-meli@users.noreply.github.com>
2020-05-02 15:03:39 -0700

    add luajit binaries and vswhere to make generation self-contained; windows-only for now

--- generator/generator.bat
+++ generator/generator.bat
@@ -9,16 +9,19 @@
 :: structs_and_enums.lua with struct and enum information-definitions
 :: impl_definitions.lua for implementation function definitions
 
-:: set your PATH if necessary for LuaJIT or Lua5.1 or luajit with: (for example)
-set PATH=%PATH%;C:\anima;C:\mingws\i686-7.2.0-release-posix-dwarf-rt_v5-rev1\mingw32\bin;
-:: set PATH=%PATH%;C:\luaGL;C:\i686-7.2.0-release-posix-dwarf-rt_v5-rev1\mingw32\bin;
-:: set PATH=%PATH%;C:\luaGL\sources\luajit-master\luajit-master\bin\mingw32;C:\mingw32\bin;
+@echo off
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+:: I think this path changed for 2019, so this may not work for older versions
+call "%InstallDir%\VC\Auxiliary\Build\vcvars64.bat"
+
 ::process  files
 :: arg[1] compiler name gcc, clang or cl
 :: arg[2] imgui_internal functions generation: true or false
 :: arg[3..n] name of implementations to generate
-luajit ./generator.lua gcc true glfw opengl3 opengl2 sdl
+.\luajit\luajit generator.lua cl true
 
 ::leave console open
-cmd /k
+pause
 
